8843f59906756f8d6fa3d7457d8105c74cf2681b
hompage0718 <johnkobz14@gmail.com>
2021-07-26 11:10:46 +0900

    initial 2

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+import Hello from "./components/views/Hello";
+
+function App() {
+  return (
+    <div className="App">
+      <Hello />
+    </div>
+  );
+}
+
+export default App;
